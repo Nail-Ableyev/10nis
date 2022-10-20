ac9112c87a8507b45203807b5a1230ea2e78f61a
Nail-Ableyev <nail.ableyev@gmail.com>
2022-10-20 23:23:32 +0600

    add netlify env

--- libs/google_sheet.js
+++ libs/google_sheet.js
@@ -1,18 +1,18 @@
 import { google } from 'googleapis';
-import {clientEmail, privKey, spreadSheet} from "./tt"
+// import {clientEmail, privKey, spreadSheet} from "./tt"
 export async function getDataFromList() {
   try {
     const target = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
     const jwt = new google.auth.JWT(
-      clientEmail,
+      process.env.NEXT_PUBLIC_GOOGLE_SHEETS_CLIENT_EMAIL,
       null,
-      (privKey || '').replace(/\\n/g, '\n'),
+      (process.env.NEXT_PUBLIC_GOOGLE_SHEETS_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
       target
     );
 
     const sheets = google.sheets({ version: 'v4', auth: jwt });
     const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: spreadSheet,
+      spreadsheetId: process.env.NEXT_PUBLIC_SPREADSHEET_ID,
       range: 'main', // sheet name
     });
 
